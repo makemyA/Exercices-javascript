ec66ffe881998b062b2ea5216fec283486db49b9
Paul Henrot <henrot.paul@gmail.com>
2018-06-14 23:44:58 +0200

    stress test javascript1

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
-alert ("hello");
-console.log ("hello");
+/* let a = 0;
+let tableau= [];
+let i=0;
+let j=0;
+for (i; i<5; i++) {
+    a=a+1;
+    tableau.push(tableau[j]);
+    for ( j; j < 6; j++){
+        tableau.push(a);
+        console.log(tableau.toString());
+    } 
+}
+
+console.log("test log");
+ */
+/* let tableau = [[]];
+let b = 0;
+let a =0;
+
+ for (  let i= 0; i<3; i++) {
+     let a = tableau[i];
+     a=a+1;
+     for( let j= 0; j<3;j++) {
+        let b =tableau[i][j]; 
+        b = b+1;
+        console.log(tableau);
+     }
+        
+    }   */
+
+    let a="";
+    let b= 0;
+    for (let i =0 ; i < 4; i++){
+        a ="";
+        b=b+1;
+        for(let j=0;j <6; j++) {
+                a=a+b;
+                console.log(a);
+        }
+           }
+   
+  
+ 
+
